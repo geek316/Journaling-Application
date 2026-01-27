73505c074b3c703f159519ec3770f2aa168b6a70
Hemant Sharan <hemant.sharan@dell.com>
2026-01-27 19:37:25 +0530

    Test cases

--- Journal App/src/test/java/net/engineeringdigest/journalapp/service/UserServiceTests.java
+++ Journal App/src/test/java/net/engineeringdigest/journalapp/service/UserServiceTests.java
@@ -1,13 +1,49 @@
 package net.engineeringdigest.journalapp.service;
 
+import net.engineeringdigest.journalapp.entity.User;
+import net.engineeringdigest.journalapp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+//@SpringBootTest
 public class UserServiceTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @Disabled
     @Test
     public void testAdd() {
-        assertEquals(4, 2+1);
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testFindByUserName() {
+        User user = userRepository.findByUserName("Hemant");
+        assertNotNull(user);
+        assertTrue(5>3);
+        assertFalse(user.getJournalEntries().isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12",
+            "3,3,9"
+    })
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a + b, "Failed for :" + expected);
     }
 }
